550453151ce4efa8ccb8a1125142b90e3787bdc4
Giovanni Giordano <giordano.giovanni1993@gmail.com>
2025-03-06 11:29:20 +0000

    Cambio pulsante

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ export const App = () => {
               className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
               onClick={() => setTitle("Charizard")}
           >
-            Cambia titolo
+            Cambia
           </button>
 
           <p className="text-center">
