5f89970a8ee6488aefb33a8381b779235d4de5c7
zengstoickevin <zhanghanyimatteo2007@gmail.com>
2025-03-13 19:40:26 +0000

    F

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,88 @@
-interface PokemonCard {
+interface PokemonCard{
   id: number;
   image: string;
   name: string;
   types: string[];
 }
-
-const data: PokemonCard[] = [
+const data= [
   {
     id: 1,
-    name: "Geodude",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    types: ["fire", "water"],
+    name: "BULBASAUR",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png ",
+    types: ["poison", "water"],
   },
+
   {
-    id: 2,
-    name: "Pikachu",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    types: ["electric"], // Corretto "Elettric" in "electric"
-  },
-  {
-    id: 3,
-    name: "Bubs",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    types: ["fire", "water"],
-  },
-  {
-    id: 4,
-    name: "Bubs",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    types: ["fire", "water"],
-  },
-  {
-    id: 5,
-    name: "Squirtle",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    types: ["water"],
-  },
-  {
-    id: 6,
-    name: "Charmander",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  id: 2,
+  name: "PIKACHU",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png ",
+  types: ["elettric"],
+},
+{
+id: 3,
+  name: "CLEFAIRY",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/35.png",
+  types: ["fire", "water"],
+},
+{
+id: 4,
+  name: "RATTATA",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png ",
+  types: ["fire", "water"],
+},
+{
+id: 5,
+  name: "SQUI LE",
+  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png ",
+  types: ["fire", "water"],
+},
+{
+  id: 6,
+    name: "CHARMANDER",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png ",
     types: ["fire"],
   },
 ];
 
-const typeColors: { [key: string]: string } = {
-  fire: "bg-red-500",
-  water: "bg-blue-500",
-  electric: "bg-yellow-500",
+const typeC: { [key: string]: string} = {
+  fire: "bg-red-300",
+  water: "bg-blue-300",
+  elettric: "bg-yellow-200",
+  poison: "bg-purple-400",
 };
 
-function getTypeColor(type: string) {
-  return typeColors[type] || "bg-gray-400"; // Colore di fallback
+function getTypeColor(type: string){
+  const color= typeC[type];
+  return color;
+}
+
+
+const Card = (props: PokemonCard) => {
+  return  <div className="flex flex-wrap bg-red-200 p-0.1 w-3xs">
+            {props.id} - {props.name}
+            <img src = {props.image}/>
+            <div className="flex flex-wrap bg-red-200 gap-2 p-3">
+              {props.types.map((type) => {
+              return <div className={`p-4 ${getTypeColor(type)}`}>{type}</div>
+             })}
+            </div>
+          </div>
+
+ 
 }
 
 export const App = () => {
   return (
-    <div className="flex flex-wrap gap-4 p-4 bg-sky-200">
-      {data.map((item) => (
-        <div key={item.id} className="bg-white p-4 shadow-md rounded-lg w-40 text-center">
-          <h3 className="text-lg font-bold">{item.id} - {item.name}</h3>
-          <img src={item.image} alt={item.name} className="w-full h-32 object-contain" />
-          <div className="flex justify-center gap-2 mt-2">
-            {item.types.map((type) => (
-              <div key={type} className={`px-2 py-1 rounded ${getTypeColor(type)}`}>
-                {type}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+  <div>
+    <div className="flex flex-wrap gap-40 p-50">
+      {data.map ((item) => {
+        return <Card id={item.id} name={item.name} image={item.image} types={item.types} />
+      })}
     </div>
+  </div>
   );
 };
 
 export const Detail = () => {
-  return null;
-};
+  return null
+}
