8117e7e6e3bb2d6f2c9ec2cb1899edd6fe804d54
zengstoickevin <zhanghanyimatteo2007@gmail.com>
2025-03-13 18:16:53 +0000

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,75 @@
-interface PokemonCard{
+import { useEffect, useState } from "react";
+import { PokeAPI } from "./pokeapiClient";
+
+interface PokemonCard {
   id: number;
   image: string;
   name: string;
   types: string[];
 }
-const data= [
-  {
-    id: 1,
-    name: "BULBASAUR",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png ",
-    types: ["poison", "water"],
-  },
 
-  {
-  id: 2,
-  name: "PIKACHU",
-  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png ",
-  types: ["elettric"],
-},
-{
-id: 3,
-  name: "CLEFAIRY",
-  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/35.png",
-  types: ["fire", "water"],
-},
-{
-id: 4,
-  name: "RATTATA",
-  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png ",
-  types: ["fire", "water"],
-},
-{
-id: 5,
-  name: "SQUI LE",
-  image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png ",
-  types: ["fire", "water"],
-},
-{
-  id: 6,
-    name: "CHARMANDER",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png ",
-    types: ["fire"],
-  },
-];
+async function fetchData(): Promise<PokemonCard[]> {
+  const data = await PokeAPI.getPokemonsList();
 
-const typeC: { [key: string]: string} = {
-  fire: "bg-red-300",
-  water: "bg-blue-300",
-  elettric: "bg-yellow-200",
-  poison: "bg-purple-400",
-};
+  const pokemons = await Promise.all(
+     data.results.map((pokemon) => {
+    return PokeAPI.getPokemonByName(pokemon.name);
+    })
+  );
 
-function getTypeColor(type: string){
-  const color= typeC[type];
+  return pokemons.map ((pokemon) => {
+    return {
+      id: pokemon.id,
+      name: pokemon.name,
+      image: pokemon.sprites.other["official-artwork"].front_default ?? "",
+      types: pokemon.types.map((t) => t.type.name),
+    };
+
+   
+  });
+}
+
+const typeColors: { [key: string]: string } = {
+  fire: "bg-red-500",
+  water: "bg-blue-500",
+  poison: "bg-purple-500"
+}; 
+function getTypeColor(type: string) {
+  const color = typeColors[type];
   return color;
 }
 
-
 const Card = (props: PokemonCard) => {
-  return  <div className="flex flex-wrap bg-red-200 p-0.1 w-3xs">
+  return  <div className="bg-white w-2xs">
             {props.id} - {props.name}
             <img src = {props.image}/>
-            <div className="flex flex-wrap bg-red-200 gap-2 p-3">
+            <div className="flex justify-center gap-4 p-4">
               {props.types.map((type) => {
               return <div className={`p-4 ${getTypeColor(type)}`}>{type}</div>
              })}
             </div>
           </div>
-
- 
 }
 
 export const App = () => {
+  const [data, setData] = useState<PokemonCard[]>([]);
+
+  useEffect(() => {
+    fetchData().then((result) => {
+      setData(
+        result.map((item) => ({
+          id: item.id,
+          name: item.name,
+          image: item.image,
+          types: item.types,
+        }))
+      );
+    });
+  }, []);
+
   return (
   <div>
-    <div className="flex flex-wrap gap-40 p-50">
+    <div className="flex flex-wrap gap-4 p-4">
       {data.map ((item) => {
         return <Card id={item.id} name={item.name} image={item.image} types={item.types} />
       })}
---
+++ src/pokeapiClient.ts
@@ -0,0 +1,5 @@
+import { Pokedex } from "pokeapi-js-wrapper";
+
+export const PokeAPI = new Pokedex ({
+    limit: 20,
+});
